dfd521e30d217c23e05efd7a8bcb2b36c64536b7
abd muiz syam <kang84339@gmail.com>
2026-06-22 13:48:03 +0700

    fix: penyelarasan akhir rute murni ke firebase

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -2,13 +2,13 @@ import { createClient } from '@base44/sdk';
 import { appParams } from '@/lib/app-params';
 
 const { appId, token, functionsVersion } = appParams;
-const FIREBASE_PROXY_URL = 'https://raport-k13-9353a.web.app';
+const FIREBASE_URL = 'https://raport-k13-9353a.web.app';
 
 export const base44 = createClient({
   appId,
   token,
   functionsVersion,
-  serverUrl: FIREBASE_PROXY_URL,
+  serverUrl: FIREBASE_URL,
   requiresAuth: false,
-  appBaseUrl: FIREBASE_PROXY_URL
+  appBaseUrl: FIREBASE_URL
 });
